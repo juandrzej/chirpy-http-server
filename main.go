7756b55db9ee29d1a65b8f18bac42301702f1c21
juandrzej <programming.yippee729@passinbox.com>
2026-01-13 23:33:51 +0100

    first lesson done

--- main.go
+++ main.go
@@ -1,4 +1,13 @@
 package main
 
+import "net/http"
+
 func main() {
+	serverHandler := http.NewServeMux()
+	server := http.Server{
+		Handler: serverHandler,
+		Addr:    ":8080",
+	}
+
+	_ = server.ListenAndServe()
 }
