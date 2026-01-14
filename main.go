a1718973a4bf228e15da38c91910cd01927b3b7e
juandrzej <programming.yippee729@passinbox.com>
2026-01-14 14:56:21 +0100

    add health and prefix

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, "OK")
+	})
 
 	server := http.Server{
 		Handler: mux,
