94b952fc83e2f28181714a782447bb49384ac250
juandrzej <programming.yippee729@passinbox.com>
2026-03-10 00:17:00 +0100

    add new pack, add func sign

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,11 @@
 package auth
 
-import "github.com/alexedwards/argon2id"
+import (
+	"time"
+
+	"github.com/alexedwards/argon2id"
+	"github.com/google/uuid"
+)
 
 func HashPassword(password string) (string, error) {
 	return argon2id.CreateHash(password, argon2id.DefaultParams)
@@ -9,3 +14,5 @@ func HashPassword(password string) (string, error) {
 func CheckPasswordHash(password, hash string) (bool, error) {
 	return argon2id.ComparePasswordAndHash(password, hash)
 }
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error)
